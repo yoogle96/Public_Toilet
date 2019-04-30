b0225841ec45c9c5953fb0f7920dd2a713408146
yoogle96 <dmltjs851@gmail.com>
2019-04-30 23:01:50 +0900

    asdf

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn Public_Toilet.wsgi --log-file -
--- Public_Toilet/settings.py
+++ Public_Toilet/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -123,3 +124,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 ) 
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
