b95d48ee2b296dd1451bcbd045394b57e975b6a1
yoogle96 <dmltjs851@gmail.com>
2019-04-30 23:28:01 +0900

    test

--- Toilet_Map/views.py
+++ Toilet_Map/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from urllib.request import urlopen
 import json
 
+def test(request):
+    return render(request, 'test.html')
+
 def home(request):
     url = 'http://api.data.go.kr/openapi/pblic-toilet-std?ServiceKey=GKrY0IlCVjdcT4cwBqkwS2Cwx9FFp7WGQRVApzmHKlhDqs8cz7swOzcUpzkEHZX6XV9TSC5YUqcM%2BtNLSndxQA%3D%3D&numOfRows=5&type=json'
     json_url = urlopen(url)
