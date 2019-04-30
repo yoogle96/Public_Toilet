29bac218d7b5cb79b53f3df4337ec698ecb66ac3
yoogle96 <dmltjs851@gmail.com>
2019-04-30 23:30:47 +0900

    asdf

--- Toilet_Map/views.py
+++ Toilet_Map/views.py
@@ -3,7 +3,7 @@ from urllib.request import urlopen
 import json
 
 def test(request):
-    return render(request, 'test.html')
+    return render(request, "test.html")
 
 def home(request):
     url = 'http://api.data.go.kr/openapi/pblic-toilet-std?ServiceKey=GKrY0IlCVjdcT4cwBqkwS2Cwx9FFp7WGQRVApzmHKlhDqs8cz7swOzcUpzkEHZX6XV9TSC5YUqcM%2BtNLSndxQA%3D%3D&numOfRows=5&type=json'
@@ -12,6 +12,6 @@ def home(request):
     print(type(json_data))
     # for i in range(0,300):
     #     print(i ,json_data['response']['body']['items'][i]['toiletNm'])
-    return render(request, 'index.html', context={'data':json_data['response']['body']['items']})
+    return render(request, "index.html", context={'data':json_data['response']['body']['items']})
 
 # Create your views here.
