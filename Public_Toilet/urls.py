f7a932f7d18bb2a8f56c92a6ee5ffa3e445817e7
yoogle96 <dmltjs851@gmail.com>
2019-04-30 23:40:48 +0900

    fix

--- Public_Toilet/urls.py
+++ Public_Toilet/urls.py
@@ -5,5 +5,4 @@ import Toilet_Map.views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', Toilet_Map.views.home, name="home"),
-    path('test/', Toilet_Map.views.test, name="test"),
 ]
