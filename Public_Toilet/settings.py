6f26402c36a9e86e8cfabc49146f9f05207d0254
yoogle96 <dmltjs851@gmail.com>
2019-04-30 23:36:19 +0900

    asdf

--- Public_Toilet/settings.py
+++ Public_Toilet/settings.py
@@ -56,7 +56,9 @@ ROOT_URLCONF = 'Public_Toilet.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR,'Toilet_Map/Templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
